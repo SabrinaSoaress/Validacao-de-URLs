13c9df1a31499918e19e41c70301e918b7c34835
Sabrina Soares <92693817+SabrinaSoaress@users.noreply.github.com>
2023-06-11 16:06:22 -0300

    Update.py

--- tela.py
+++ tela.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import requests
 import socket
+import whois
 import urllib.parse
 from tkinter import messagebox
 from tkinter import filedialog
@@ -61,12 +62,6 @@ def validar_urls():
     
     messagebox.showinfo("Histórico de URLs", f"URLs válidas:\n{historico_urls_validas}\n\nURLs inválidas:\n{historico_urls_invalidas}")
 
-    # Abre a tela de detalhes quando o botão "Detalhes" é clicado
-    selected_index = listbox_urls.curselection()
-    if selected_index:
-        url = listbox_urls.get(selected_index)[1]
-        exibir_detalhes(url)
-
 def importar_urls():
     file_path = filedialog.askopenfilename(filetypes=[("Arquivos de Texto", "*.txt")])
     if file_path:
@@ -120,61 +115,91 @@ def exibir_urls():
     for url in urls:
         listbox_urls.insert(tk.END, url)
     conn.close()
+    
+def exibir_historico():
+    historico_window = tk.Toplevel(window)
+    historico_window.title("Histórico de Validações")
+    historico_window.geometry("600x500")
+    historico_window.configure(background='pink')
 
-    # Adiciona o botão "Detalhes"
-    button_detalhes = tk.Button(window, text="Detalhes", command=validar_urls, bg='black', fg='pink', font=(12))
-    button_detalhes.pack()
+    listbox_historico = tk.Listbox(historico_window, width=60, font=(20))
+    listbox_historico.pack()
 
-def exibir_detalhes(url):
-    # Realiza a validação da URL
-    if not validar_url(url):
-        messagebox.showerror("Erro", "URL inválida")
-        return
-    
-    # Obtém o tempo de resposta da conexão
-    try:
-        response = requests.get(url)
-        tempo_resposta = response.elapsed.total_seconds()
-    except requests.exceptions.RequestException:
-        messagebox.showerror("Erro", "Não foi possível obter o tempo de resposta da conexão")
-        return
-    
-    # Obtém o endereço IP do servidor que respondeu à validação
-    try:
-        parsed_url = urllib.parse.urlparse(url)
-        host = parsed_url.netloc
-        endereco_ip = socket.gethostbyname(host)
-    except:
-        messagebox.showerror("Erro", "Não foi possível obter o endereço IP do servidor")
-        return
-    
-    # Obtém o cabeçalho devolvido
-    cabeçalho = response.headers
-    
-    # Exibe os detalhes em uma nova janela
-    detalhes_window = tk.Toplevel(window)
-    detalhes_window.title("Detalhes da Conexão")
-    detalhes_window.geometry("400x300")
-    detalhes_window.configure(background='pink')
-    
-    # Cria os rótulos para exibir os detalhes
-    label_tempo_resposta = tk.Label(detalhes_window, text="Tempo de Resposta: {:.2f} segundos".format(tempo_resposta), background='pink', font=(12))
-    label_tempo_resposta.pack(pady=10)
-    
-    label_endereco_ip = tk.Label(detalhes_window, text="Endereço IP do Servidor: {}".format(endereco_ip), background='pink', font=(12))
-    label_endereco_ip.pack()
-    
-    label_cabecalho = tk.Label(detalhes_window, text="Cabeçalho:", background='pink', font=(12))
-    label_cabecalho.pack(pady=10)
-    
-    text_cabecalho = tk.Text(detalhes_window)
-    text_cabecalho.insert(tk.END, str(cabeçalho))
-    text_cabecalho.pack(pady=10, expand=True, fill=tk.BOTH)
-    
-    detalhes_window.mainloop()
+    historico_urls_validas, historico_urls_invalidas = obter_historico_validacoes()
 
+    listbox_historico.insert(tk.END, "URLs Válidas:")
+    for url in historico_urls_validas:
+        listbox_historico.insert(tk.END, url)
+        
+    listbox_historico.insert(tk.END, "")
+    listbox_historico.insert(tk.END, "URLs Inválidas:")
+    for url in historico_urls_invalidas:
+        listbox_historico.insert(tk.END, url)
 
-# Cria a tabela no banco de dados (se ainda não existir)
+    historico_window.mainloop()
+
+def obter_historico_validacoes():
+    historico_urls_validas = []
+    historico_urls_invalidas = []
+
+    conn = sqlite3.connect("urls.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM urls")
+    urls = cursor.fetchall()
+    conn.close()
+
+    for url in urls:
+        if validar_url(url[1]):
+            historico_urls_validas.append(url[1])
+        else:
+            historico_urls_invalidas.append(url[1])
+
+    return historico_urls_validas, historico_urls_invalidas
+
+def exibir_detalhes():
+    selected_index = listbox_urls.curselection()
+    if selected_index:
+        url = listbox_urls.get(selected_index)[1]
+        if not validar_url(url):
+            messagebox.showerror("Erro", "URL inválida")
+            return
+        try:
+            response = requests.get(url)
+            tempo_resposta = response.elapsed.total_seconds()
+        except requests.exceptions.RequestException:
+            messagebox.showerror("Erro", "Não foi possível obter o tempo de resposta da conexão")
+            return
+
+        try:
+            parsed_url = urllib.parse.urlparse(url)
+            host = parsed_url.netloc
+            endereco_ip = socket.gethostbyname(host)
+        except:
+            messagebox.showerror("Erro", "Não foi possível obter o endereço IP do servidor")
+            return
+
+        cabeçalho = response.headers
+
+        detalhes_window = tk.Toplevel(window)
+        detalhes_window.title("Detalhes da Conexão")
+        detalhes_window.geometry("600x500")
+        detalhes_window.configure(background='pink')
+
+        label_tempo_resposta = tk.Label(detalhes_window, text="Tempo de Resposta: {:.2f} segundos".format(tempo_resposta), background='pink', font=(12))
+        label_tempo_resposta.pack(pady=10)
+        
+        label_endereco_ip = tk.Label(detalhes_window, text="Endereço IP do Servidor: {}".format(endereco_ip), background='pink', font=(12))
+        label_endereco_ip.pack()
+        
+        label_cabecalho = tk.Label(detalhes_window, text="Cabeçalho:", background='pink', font=(12))
+        label_cabecalho.pack(pady=10)
+        
+        text_cabecalho = tk.Text(detalhes_window)
+        text_cabecalho.insert(tk.END, str(cabeçalho))
+        text_cabecalho.pack(pady=10, expand=True, fill=tk.BOTH)
+
+        detalhes_window.mainloop()
+
 criar_tabela()
 
 # Cria a janela principal
@@ -193,38 +218,42 @@ rotulo_boas_vindas = tk.Label(
 rotulo_boas_vindas.pack(pady=300)
 
 botao_telaurl = tk.Button(
-    tela_boas_vindas, text="Ir para Pagina de URLs", command=exibir_telas, bg='black', fg='pink', font=(12))
+    tela_boas_vindas, text="Ir para Pagina de URLs", command=exibir_telaUrl, bg='black', fg='pink', font=(12))
 botao_telaurl.pack()
 
-# Cria os elementos da interface
 tela_url = tk.Frame(window)
 tela_url.configure(background='pink')
 tela_url.pack(side=tk.TOP)
 
+label_url = tk.Label(tela_url, text="URL:", background='pink', font=(20))
+label_url.pack(pady=30)
 
-label_url = tk.Label(window, text="URL:", background='pink', font=(20))
-label_url.pack()
-
-entry_url = tk.Entry(window)
+entry_url = tk.Entry(tela_url, width=60, font=(20))
 entry_url.pack()
 
-button_adicionar = tk.Button(window, text="Validar e Adicionar", command=adicionar_url, bg='black', fg='pink', font=(12))
+button_adicionar = tk.Button(tela_url, text="Validar e Adicionar", command=adicionar_url, bg='black', fg='pink', font=(12))
 button_adicionar.pack()
 
-button_alterar = tk.Button(window, text="Alterar", command=alterar_url, bg='black', fg='pink', font=(12))
+button_alterar = tk.Button(tela_url, text="Alterar", command=alterar_url, bg='black', fg='pink', font=(12))
 button_alterar.pack()
 
-button_excluir = tk.Button(window, text="Excluir", command=excluir_url, bg='black', fg='pink', font=(12))
+button_excluir = tk.Button(tela_url, text="Excluir", command=excluir_url, bg='black', fg='pink', font=(12))
 button_excluir.pack()
 
-listbox_urls = tk.Listbox(window)
+listbox_urls = tk.Listbox(tela_url, width=60, font=(20))
 listbox_urls.pack()
 
-button_validar = tk.Button(window, text="Validar URLs", command=validar_urls, bg='black', fg='pink', font=(12))
+button_validar = tk.Button(tela_url, text="Validar URLs", command=validar_urls, bg='black', fg='pink', font=(12))
 button_validar.pack()
 
-button_importar = tk.Button(window, text="Importar", command=importar_urls, bg='black', fg='pink', font=(12))
+button_importar = tk.Button(tela_url, text="Importar", command=importar_urls, bg='black', fg='pink', font=(12))
 button_importar.pack()
 
-exibir_urls()
+button_detalhes = tk.Button(tela_url, text="Detalhes", command=exibir_detalhes, bg='black', fg='pink', font=(12))
+button_detalhes.pack()
+
+button_historico = tk.Button(tela_url, text="Histórico de Validações", command=exibir_historico, bg='black', fg='pink', font=(12))
+button_historico.pack()
+
+exibir_telaUrl()
 window.mainloop()
